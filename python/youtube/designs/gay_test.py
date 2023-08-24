32806fc9f9d70add6fae0619a07fc0e3ec6d7fbb
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-24 07:42:42 -0700

    updated `gay_test.py`

--- python/youtube/designs/gay_test.py
+++ python/youtube/designs/gay_test.py
@@ -4,12 +4,12 @@ import random
 
 
 def no():
-    messagebox.showinfo('', 'میدونستم داش')
-    messagebox.showinfo('', 'مرسی که بهم ثابت کردی')
+    messagebox.showinfo('', 'I knew :-)')
+    messagebox.showinfo('', 'Thanks for agreeing :-)')
     quit()
 
 
-def motionMouse(event):
+def motion_mouse(event):
     btnYes.place(x=random.randint(0, 500), y=random.randint(0, 500))
 
 
@@ -22,7 +22,7 @@ label = Label(root, text='Are you gay?', font='Peyda 20 bold', bg='white')
 label.pack()
 btnYes = Button(root, text='No', font='Peyda 15 bold', cursor='hand2')
 btnYes.place(x=170, y=100)
-btnYes.bind('<Enter>', motionMouse)
+btnYes.bind('<Enter>', motion_mouse)
 btnNo = Button(root, text='Yes', font='Peyda 15 bold', command=no, cursor='hand2')
 btnNo.place(x=350, y=100)
 root.mainloop()
